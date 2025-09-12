18f9349f0d10c1c0e7f2aa8a27329d3d5d42f8de
TRIPLE167 <153914342+TRIPLE167@users.noreply.github.com>
2025-09-12 21:24:02 +0400

    changed import

--- linkUp/src/components/auth/Verification.jsx
+++ linkUp/src/components/auth/Verification.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from "react";
-import "../../styles/Verification.scss";
+import "../../styles/verification.scss";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
