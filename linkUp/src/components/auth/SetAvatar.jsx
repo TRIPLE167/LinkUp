4dda53ed7b11dd305a0312b6b2da9378cf0ab6c3
TRIPLE167 <153914342+TRIPLE167@users.noreply.github.com>
2025-09-12 21:36:04 +0400

    changed imports

--- linkUp/src/components/auth/SetAvatar.jsx
+++ linkUp/src/components/auth/SetAvatar.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "../../styles/setAvatar.scss";
+import "../../styles/SetAvatar.scss";
 import AvatarCrop from "../app/AvatarCrop/AvatarCrop";
 import { getCroppedImg } from "../app/AvatarCrop/getCroppedImg";
 
